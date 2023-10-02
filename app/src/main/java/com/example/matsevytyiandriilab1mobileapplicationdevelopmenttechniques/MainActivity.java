cc40c0c9e0b7bdade9702e559f8a3d3182feb0d6
andriimatsevytyi <macevitiya@i.ua>
2023-10-02 16:33:41 -0400

    LAB 1 completed. Two buttons that change color and text added. Project tested.

---
+++ app/src/main/java/com/example/matsevytyiandriilab1mobileapplicationdevelopmenttechniques/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.matsevytyiandriilab1mobileapplicationdevelopmenttechniques;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initializing
+        Button btnChangeColor = findViewById(R.id.btnChangeColor);
+        Button btnChangeText = (Button) findViewById(R.id.btnChangeText);
+        this.textView = findViewById(R.id.textView);
+
+        // Changing the color
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setTextColor(Color.parseColor("RED"));
+            }
+        });
+
+        btnChangeText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText("Heroyam Slava!");
+            }
+        });
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Matsevytyi Andrii LAB1 Mobile application development techniques"
+include(":app")
